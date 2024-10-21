5b29afac3d6735da1c1995a722d88e9b59e88a7f
lebedeva-svetlana <svetlana-swanson@yandex.ru>
2024-10-21 16:19:40 +0300

    Added periodic launch functionality.

--- Program.cs
+++ Program.cs
@@ -14,6 +14,22 @@ var logger = new LoggerConfiguration()
 
 //Log.CloseAndFlush();
 
+if (args.Length == 0)
+{
+    Console.WriteLine("Enter the frequency argument of post publication in minutes.");
+}
+
+bool isSuccess = int.TryParse(args[0], out int freqMinutes);
+if (!isSuccess)
+{
+    Console.WriteLine("The frequency argument entered in incorrect format.");
+}
+
 ConfigurationHelper helper = new(configuration);
 Bot bot = new(helper);
-await bot.Send();
+
+while (true)
+{
+    await bot.Send();
+    Thread.Sleep(freqMinutes * 1000 * 60);
+}
